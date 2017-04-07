55bf9124e78091495f3f3787bc348e16048c73ab
Bobby Yang <122@holbertonschool.com>
2017-04-07 08:20:06 +0000

    updated with new test string

--- main.c
+++ main.c
@@ -1,15 +1,6 @@
 #include "header.h"
 #include "tokenizer.h"
 
-token_types token_names[] = {
-    { TOKEN_SEMICOLON,  ";",  "semicolon" },
-    { TOKEN_PIPE,       "|",  "pipe" },
-    { TOKEN_REWRITE,    ">",  "rewrite" },
-    { TOKEN_APPEND,     ">>", "append" },
-    { TOKEN_CAT,        "<",  "cat" },
-    { TOKEN_BACKGROUND, "&",  "background" }
-};
-
 /**
  * main - entry point
  * @argc: count of arguments
@@ -22,7 +13,7 @@ int main(int __attribute__((unused))argc, char __attribute__((unused))*argv[])
 	tokens_t tokens;
 	char *str;
 
-	str = "The \"quick brown\" fox\\ ju;mps ;;;o\"ver th\"e lazy do\\\"g.";
+	str = "background & echo \"/bin/ls\" | ./hsh > cmd 2 >> cmd 3; cat < somefile";
 
 	printf("Original String: %s\n", str);
 	printf("========================================================\n");
